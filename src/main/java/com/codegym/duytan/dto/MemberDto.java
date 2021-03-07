7998e7870651f6c920ca31040551d2461179d403
dqviet <dinhv2388@gmail.com>
2021-03-07 17:19:08 +0700

    merge code

--- src/main/java/com/codegym/duytan/dto/MemberDto.java
+++ src/main/java/com/codegym/duytan/dto/MemberDto.java
@@ -5,8 +5,6 @@ import lombok.Data;
 @Data
 public class MemberDto {
 
-    // Xem lại convention đặt tên của Java. Cách tên này là của JS
-
     private Integer member_id;
     private String name;
     private String email;
